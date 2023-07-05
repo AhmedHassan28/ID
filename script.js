ebe6628f0c84b9e8d73651b32757cebf1f404fb1
AhmedHassan28 <135166942+AhmedHassan28@users.noreply.github.com>
2023-07-05 22:42:55 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,25 @@
-const fileInput = document.querySelector("input");
+const fileInput = document.querySelector("input"),
 downloadBtn = document.querySelector("button");
 
 downloadBtn.addEventListener("click", e => {
-    e.preventDefault(); // preventing form from submitting
+    e.preventDefault();
     downloadBtn.innerText = "Downloading file...";
     fetchFile(fileInput.value);
 });
 
 function fetchFile(url) {
-    // fetching file & returning response as blob
     fetch(url).then(res => res.blob()).then(file => {
-        // URL.createObjURL creates a url of passed object
         let tempUrl = URL.createObjectURL(file);
-        let aTag = document.createElement("a");
-        aTag.href = tempUrl; // passing tempUrl as href value of <a> tag
-        // passing file last name & extension as download value of <a> tag
+        const aTag = document.createElement("a");
+        aTag.href = tempUrl;
         aTag.download = url.replace(/^.*[\\\/]/, '');
-        document.body.appendChild(aTag); // adding <a> tag inside body
-        aTag.click(); // clicking <a> tag so the file download
-        aTag.remove(); // removing <a> tag once file downloaded
-        URL.revokeObjectURL(tempUrl); // removing tempURL from the document
+        document.body.appendChild(aTag);
+        aTag.click();
         downloadBtn.innerText = "Download File";
+        URL.revokeObjectURL(tempUrl);
+        aTag.remove();
     }).catch(() => {
-        // catch method will call if any error comes during downloading
-        downloadBtn.innerText = "Download File";
         alert("Failed to download file!");
+        downloadBtn.innerText = "Download File";
     });
 }
